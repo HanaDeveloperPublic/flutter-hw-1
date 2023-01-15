329e26cc296c99fcfd04f717739a467ae23e1fd9
HanaDeveloperPublic <122750994+HanaDeveloperPublic@users.noreply.github.com>
2023-01-16 00:01:02 +0300

    My First HW
    Simple Calculator

---
+++ calculator.dart
@@ -0,0 +1,21 @@
+main()
+{
+    const x= 10;
+    const y= 5;
+    const sum = x+y;
+    const sub = x-y;
+    const div = x/y;
+    const mod = x/y;
+   
+    print("The First Value Is $x");
+    print("The Secoand Value Is $y");
+    print("Simple Clculator Is Run Now ...........");
+    print("The Result Of Combination Result Is $sum");
+    print("............................................");
+    print("The Result Of Subtraction Result Is $sub");
+    print("............................................");
+    print("The Result Of Division Result Is $div");
+
+
+    
+}
